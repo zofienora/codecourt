66d8de98a8afc0c85a112dde4c483949250dd4d3
Zofia Nora <sophienora92@gmail.com>
2025-03-21 18:51:34 +0100

    added basic content coming soon

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Main from './components/Main'
 
 function App() {
 
   return (
     <>
-   
+      <Main />
+
     </>
   )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,20 @@
+
+
+function Main() {
+
+    return (
+      <>
+        <main>
+            <div className="container">
+                <h1>Like a Basketball Court, but for Coding.</h1>
+                <p>Coming soon...</p>
+            </div>
+        
+        </main>
+     
+      </>
+    )
+  }
+  
+  export default Main;
+
