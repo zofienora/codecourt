d2e2bdee5b23b53b4312faf9213d86a15c17680e
Zofia Nora <sophienora92@gmail.com>
2025-03-30 16:10:37 +0200

    projects links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,15 +6,15 @@ function Projects() {
         <div className="container">
             <div className="containerProjects">
                 <div className="project">
-                    <a href="" className="linkProject">
-                        <img src={Drag} alt="" />
+                    <a href="https://draganddropv1.vercel.app/" className="linkProject" target="_blank" rel="noopener noreferrer">
+                        <img src={Drag} alt="Drag and Dop Project" />
                     </a>
                     <p>Drag and Drop</p>
                 </div>
 
                 <div className="project">
-                    <a href="" className="linkProject">
-                        <img src={Float} alt="" />
+                    <a href="https://calculator-taupe-zeta-28.vercel.app/" className="linkProject" target="_blank" rel="noopener noreferrer">
+                        <img src={Float} alt="Floating Circle Project" />
                     </a>
                     <p>Floating Circle</p>
                 </div>
