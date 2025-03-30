89d3ea3d1ac30137a7711850ac63eb2954650d8d
Zofia Nora <sophienora92@gmail.com>
2025-03-30 15:34:27 +0200

    basic structure to projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import Main from './components/Main'
+import Projects from './components/Projects'
 
 function App() {
 
   return (
     <>
       <Main />
-
+      <Projects />
     </>
   )
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,28 @@
+
+
+function Projects() {
+    return(
+        <div className="container">
+            <div className="project">
+                <a href="" className="linkProject">
+                    <img src="" alt="" />
+                </a>
+                <p>Drag and Drop</p>
+            </div>
+
+            <div className="project">
+                <a href="" className="linkProject">
+                    <img src="" alt="" />
+                </a>
+                <p>Floating Circle</p>
+            </div>
+
+       
+
+        </div>
+    )
+
+
+}
+
+export default Projects;
