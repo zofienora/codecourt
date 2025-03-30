798bcdf790ff8642e9b38dc475e5da03c258c42d
Zofia Nora <sophienora92@gmail.com>
2025-03-30 16:02:18 +0200

    grid and media query

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,24 +1,27 @@
-
+import Drag from '../assets/drag.png';
+import Float from '../assets/float.png';
 
 function Projects() {
     return(
         <div className="container">
-            <div className="project">
-                <a href="" className="linkProject">
-                    <img src="" alt="" />
-                </a>
-                <p>Drag and Drop</p>
+            <div className="containerProjects">
+                <div className="project">
+                    <a href="" className="linkProject">
+                        <img src={Drag} alt="" />
+                    </a>
+                    <p>Drag and Drop</p>
+                </div>
+
+                <div className="project">
+                    <a href="" className="linkProject">
+                        <img src={Float} alt="" />
+                    </a>
+                    <p>Floating Circle</p>
+                </div>
+
+        
+
             </div>
-
-            <div className="project">
-                <a href="" className="linkProject">
-                    <img src="" alt="" />
-                </a>
-                <p>Floating Circle</p>
-            </div>
-
-       
-
         </div>
     )
 
@@ -26,3 +29,4 @@ function Projects() {
 }
 
 export default Projects;
+
